df542be3a1a5140c584575411813df0cf59af5db
Rodrigozura <rodrigozura@gmail.com>
2021-03-17 18:06:15 -0300

    Corregi error de .env
    Lo habia colocado en la carpeta src, y en realidad va en la raiz

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -81,5 +80,11 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-    ]
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3005,
+    }
 }
